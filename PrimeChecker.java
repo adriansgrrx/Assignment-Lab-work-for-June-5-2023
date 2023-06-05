ffe0f4a55b1ee91c79c582ab229fc5e57aaa19ba
Bugs Bunny <bugs@gmail.com>
2023-06-05 13:45:48 +0800

    Activity No.2 - Code Finalized

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,23 +1,23 @@
 public class PrimeChecker {
     public static void main(String[] args) {
-        int num = 29; // this is the number to test
+        int num = 1; // The number to test
         boolean isPrime = true;
-        /*
-        Insert your code here
-        */
-        for(int i = 2; i < num; i++){
-            if (num % i == 0) {
-                isPrime = false;
-                break;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
         }
-        if(isPrime) {
+
+        if (isPrime) {
             System.out.println(num + " is a prime number.");
         } else {
             System.out.println(num + " is not a prime number.");
         }
-    }        
+    }
 }
-
-
-
