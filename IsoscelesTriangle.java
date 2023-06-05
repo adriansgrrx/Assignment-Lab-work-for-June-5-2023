82e00c2108d2ee8fea90c07aa2d5c32106cb10bf
Bugs Bunny <bugs@gmail.com>
2023-06-05 16:12:29 +0800

    Activity No. 4 Finished

---
+++ IsoscelesTriangle.java
@@ -0,0 +1,20 @@
+public class IsoscelesTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
