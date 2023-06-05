fa0efaa0ab467d700140fabccbcdb2a927a9e3a4
Bugs Bunny <bugs@gmail.com>
2023-06-05 16:21:44 +0800

    Activity No.3 Finalized

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,10 +3,10 @@ public class FibonacciSeries {
     public static void main(String[] args) {
         int n = 10;
         int t1 = 0, t2 = 1;
-        
+
         System.out.print("First " + n + " terms: ");
 
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 0; i <= n; ++i) {
             System.out.print(t1 + " ");
 
             int sum = t1 + t2;
